1c7c60b95e8be6dbab3b0c8117490e4639cb1ef3
faysal-zx <zxfaysalgrp@gmail.com>
2020-08-28 18:09:15 +0800

    Updates

---
+++ precache-manifest.a399f8a5db11aab18098a2614fb61d2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299ea7b57b0ae9db0fe04bba0639aa14",
+    "url": "/online-class/index.html"
+  },
+  {
+    "revision": "bc3cd5a3b8ac82145c2f",
+    "url": "/online-class/static/css/main.c370e523.chunk.css"
+  },
+  {
+    "revision": "7729b52f4bbd9a49e2b4",
+    "url": "/online-class/static/js/2.f4c881aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-class/static/js/2.f4c881aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3cd5a3b8ac82145c2f",
+    "url": "/online-class/static/js/main.e3777c38.chunk.js"
+  },
+  {
+    "revision": "6fd8ca8219fcaf9c74c2",
+    "url": "/online-class/static/js/runtime-main.242a2040.js"
+  },
+  {
+    "revision": "575dac4d81c80fe9d04d165fc7dad27f",
+    "url": "/online-class/static/media/logo-coral.575dac4d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/online-class/static/media/logo.5d5d9eef.svg"
+  }
+]);
